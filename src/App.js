18d72c496303eb9c7d136cc4a6c05010978339a6
Peter <oliverdog4@gmail.com>
2020-02-15 20:09:37 -0500

    first

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <header>BakersDozen</header>
+        <main>Feed</main>
+        <section>Message Board</section>
+        <aside>Settings</aside>
+        <footer>links and about</footer>
+    </div>
+  );
+}
+
+export default App;
