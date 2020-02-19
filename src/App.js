738741a486a1505c7ed16f0642122c4709218a12
Peter <oliverdog4@gmail.com>
2020-02-18 19:48:39 -0500

    component building

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Login from './components/Login/Login';
+import Footer from './components/Footer/Footer';
+
 
 function App() {
   return (
     <div className="App">
-        {/* <Header/>
-        <Feed/>
-        <Footer/> */}
+        <Navigation/>
+        <Login/>
+        <Footer/>
     </div>
   );
 }
